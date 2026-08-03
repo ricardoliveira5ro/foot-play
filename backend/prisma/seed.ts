71efde07fd78bbf51065ef9a40db36f5f3ea3da9
Ricardo OLiveira <ricardoliveira5ro@gmail.com>
2026-08-03 18:42:28 +0100

    Process clubs from clubs.csv

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -1,23 +1,53 @@
 import { readFile } from 'fs/promises';
+import { createReadStream } from 'fs';
+import { parse } from 'csv-parse';
 import path from 'path';
 
-async function getCuratedTeams(): Promise<string[]> {
-    try {
-        const data = JSON.parse(await readFile(path.join(__dirname, '../../scripts/curated-teams.json'), 'utf8'));
+interface CuratedTeams {                                                  
+    clubIds: { id: number; name: string }[];                              
+    nationalTeamIds: { id: number; name: string }[];                      
+} 
 
-        const clubs = data.clubIds.map((c: { id: string; }) => c.id);
-        const nations = data.nationalTeamIds.map((n: { id: string; }) => n.id);
-    
-        return [...clubs, ...nations];
-    } catch (err) {
-        console.error(`Error parsing curated teams: ${err}`);
+async function getCuratedTeams() {
+    const data = JSON.parse(await readFile(path.join(__dirname, '../../scripts/curated-teams.json'), 'utf8')) as CuratedTeams;
+
+    const clubIds = data.clubIds.map(c => Number(c.id));
+    const nationsIds = data.nationalTeamIds.map(n => Number(n.id));
+
+    return {                                                              
+        clubIds,                                                          
+        nationsIds,                                                  
+        allowedTeamIds: new Set<number>([...clubIds, ...nationsIds]),
+    }; 
+}
+
+async function processClubsDataset(clubIds: Set<Number>, clubs: {}[], clubNameById: Map<number, string>): Promise<void> {
+    const parser = createReadStream(path.join(__dirname, '../../scripts/data/clubs.csv')).pipe(
+        parse({ columns: true, relax_column_count: true })
+    );
+
+    for await (const row of parser) {
+        const clubId = Number(row.club_id);
+
+        if (clubIds.has(clubId)) {
+            clubs.push({ clubId, name: row.name, domesticCompetitionId: row.domestic_competition_id || null })
+        }
+
+        clubNameById.set(clubId, row.name);
     }
-
-    return [];
 }
 
 async function main(): Promise<void> {
-    await getCuratedTeams();
+    const curatedTeams = await getCuratedTeams();
+
+    const clubIds = new Set<number>(curatedTeams.clubIds);
+    const nationsIds = new Set<number>(curatedTeams.nationsIds);
+
+    const clubs: { clubId: number; name: string; domesticCompetitionId: string | null }[] = [];
+    const clubNameById = new Map<number, string>();
+    
+    await processClubsDataset(clubIds, clubs, clubNameById);
+
 }
 
 main();
