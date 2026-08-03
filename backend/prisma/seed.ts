a39c28f7bbe12491c5b38dffb2c6600c29ead3cb
Ricardo OLiveira <ricardoliveira5ro@gmail.com>
2026-08-03 20:50:51 +0100

    Process games csv (first iteration)

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -30,10 +30,43 @@ async function processClubsDataset(clubIds: Set<Number>, clubs: {}[], clubNameBy
         const clubId = Number(row.club_id);
 
         if (clubIds.has(clubId)) {
-            clubs.push({ clubId, name: row.name, domesticCompetitionId: row.domestic_competition_id || null })
+            clubs.push({ clubId, name: row.name })
         }
 
         clubNameById.set(clubId, row.name);
+    }
+}
+
+async function processNationsDataset(nationsIds: Set<Number>, nations: {}[], nationNameById: Map<number, string>): Promise<void> {
+    const parser = createReadStream(path.join(__dirname, '../../scripts/data/national_teams.csv')).pipe(
+        parse({ columns: true, relax_column_count: true })
+    );
+
+    for await (const row of parser) {
+        const nationId = Number(row.national_team_id);
+
+        if (nationsIds.has(nationId)) {
+            nations.push({ nationId, name: row.name })
+        }
+
+        nationNameById.set(nationId, row.name);
+    }
+}
+
+async function processGamesDataset(allowedTeamIds: Set<Number>, candidateGames: {}[]): Promise<void> {
+    const parser = createReadStream(path.join(__dirname, '../../scripts/data/games.csv')).pipe(
+        parse({ columns: true, relax_column_count: true })
+    );
+
+    for await (const row of parser) {
+        const homeClubId = Number(row.home_club_id);
+        const awayClubId = Number(row.away_club_id);
+
+        if (allowedTeamIds.has(homeClubId)) {
+            candidateGames.push({ gameId: row.game_id, competitionId: row.competition_id, season: row.season || null, round: row.round || null, date: row.date || null, targetTeamId: homeClubId, homeClubId, awayClubId, homeClubGoals: row.home_club_goals || null, awayClubGoals: row.away_club_goals || null, homeClubFormation: row.home_club_formation || null, awayClubFormation: row.away_club_formation || null, stadium: row.stadium || null })
+        } else if (allowedTeamIds.has(awayClubId)) {
+            candidateGames.push({ gameId: row.game_id, competitionId: row.competition_id, season: row.season || null, round: row.round || null, date: row.date || null, targetTeamId: awayClubId, homeClubId, awayClubId, homeClubGoals: row.home_club_goals || null, awayClubGoals: row.away_club_goals || null, homeClubFormation: row.home_club_formation || null, awayClubFormation: row.away_club_formation || null, stadium: row.stadium || null })
+        }
     }
 }
 
@@ -42,12 +75,19 @@ async function main(): Promise<void> {
 
     const clubIds = new Set<number>(curatedTeams.clubIds);
     const nationsIds = new Set<number>(curatedTeams.nationsIds);
+    const allowedTeamIds = new Set<number>(curatedTeams.allowedTeamIds);
 
-    const clubs: { clubId: number; name: string; domesticCompetitionId: string | null }[] = [];
+    const clubs: { clubId: number; name: string }[] = [];
     const clubNameById = new Map<number, string>();
+    const nations: { nationId: number; name: string }[] = [];
+    const nationNameById = new Map<number, string>();
     
     await processClubsDataset(clubIds, clubs, clubNameById);
+    await processNationsDataset(nationsIds, nations, nationNameById);
 
+    const candidateGames: { gameId: number, competitionId: string, season: string, round: string, date: string, targetTeamId: number, homeClubId: number, awayClubId: number, homeClubGoals: number, awayClubGoals: number, homeClubFormation: string, awayClubFormation: string, stadium: string }[] = [];
+
+    await processGamesDataset(allowedTeamIds, candidateGames)
 }
 
 main();
